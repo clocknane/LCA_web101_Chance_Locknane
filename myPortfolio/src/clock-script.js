e4312d0bbb480717c771fe20a893ab9cb777b031
clocknane <clocknane1981@gmail.com>
2019-08-28 00:20:29 -0500

    src

--- myPortfolio/src/clock-script.js
+++ myPortfolio/src/clock-script.js
@@ -15,11 +15,7 @@ hours = (hours > 12) ? hours - 12 : hours;
 
 
 // Pad the Hours, Minutes, and Seconds with Leading Zeros
-<<<<<<< HEAD
 hours = ( "0" + hours).slice(-2);
-=======
-hours = ("0" + hours).slice(-2);
->>>>>>> 3ff202ce36e515ff89e1237a45005cdbaaf8768f
 minutes = ("0" + minutes).slice(-2);
 seconds = ("0" + seconds).slice(-2);
 
